b3ce938998b8f7a06139e812d57b1400d821248c
evelyn gomez <evelyngomezfrez@gmail.com>
2021-08-23 20:20:44 -0400

    fixed credentials

--- config/environment.rb
+++ config/environment.rb
@@ -12,14 +12,14 @@ ActionMailer::Base.smtp_settings = {
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-
-ActionMailer::Base.smtp_settings = {
-    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    :password => 'access_sendgrip_key', # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'fullstackbroker.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+#otra forma de encriptar mis credenciales mediante credentials.yml rails
+# ActionMailer::Base.smtp_settings = {
+#     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+#     :password => Rails.application.credentials.sendgrid[:access_sendgrid_key], # This is the secret sendgrid API key which was issued during API key creation
+#     :domain => 'fullstackbroker.herokuapp.com',
+#     :address => 'smtp.sendgrid.net',
+#     :port => 587,
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+#   }
   
