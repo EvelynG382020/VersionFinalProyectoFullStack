4f37db43e75fe9deeee2b902daef21e5077796d4
evelyn gomez <evelyngomezfrez@gmail.com>
2021-08-27 11:38:25 -0400

    config env for facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:twitter, :facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
   has_many :completeds, dependent: :destroy
   enum role: [:broker, :asistant, :admin]
