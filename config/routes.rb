785be55baadde972182ecb01c3cece379f47f6f5
evelyn gomez <evelyngomezfrez@gmail.com>
2021-09-09 19:30:27 -0300

    reviews final details

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     omniauth_callbacks: 'omniauth_callbacks'}
    
-  #match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
+  
     
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
